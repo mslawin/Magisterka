0cf97ad9363e1ef2b03cfcebfb8f0b28fbbabf01
mslawin <maciej1slawinski@gmail.com>
2014-06-14 22:32:42 +0200

    Generowanie listy miast z wykazu urzedowego

--- Stemmer/WykazNazw.py
+++ Stemmer/WykazNazw.py
@@ -1,10 +1,86 @@
-#coding=UTF-8
-__author__ = 'maciej'
+# coding=UTF-8
+import codecs
+import sys
+
+from plp import PLP
+
+from stemmer import Stemmer
 
 
-f = file('wykaz.txt')
-i = 0
-for line in f:
+__author__ = 'mslawin'
 
-    if i % 7 == 0:
+from bs4 import BeautifulSoup
+import re
 
+
+class TestPreparer:
+    def __init__(self):
+        self.pattern = re.compile('[a-zA-Z]+')
+
+    def start(self):
+        city_map = []
+        soup = BeautifulSoup(open('../urzedowy_wykaz/strona1.html'))
+        for p in soup.find_all('p'):
+            prev = p.span.text
+            spans = p.find_all('span')
+            last_item = self.get_span_with_hyphen(spans)
+            if last_item is not None:
+                if self.is_city(prev):
+                    if not city_map.__contains__((prev, last_item.text)):
+                        city_map.append((prev, last_item.text))
+        return self.get_forms(city_map)
+
+    def get_span_with_hyphen(self, spans):
+        for i in reversed(range(spans.__len__())):
+            s = spans.__getitem__(i)
+            if s.text.startswith('-'):
+                return s
+
+    def is_city(self, city):
+        return self.pattern.match(city)
+
+    def get_forms(self, cities):
+        city_map = dict()
+        for city_tuple in cities:
+            city_parts = city_tuple[0].split(' ')
+            ending_parts = city_tuple[1].split(' ')
+            form_parts = ''
+            for i in range(city_parts.__len__()):
+                form_parts = form_parts + self.get_form(city_parts[i], ending_parts[i]) + ' '
+            city_map[city_tuple[0]] = form_parts.strip()
+        return city_map
+
+    def get_form(self, city, ending):
+        ending = ending.replace('-', '')
+        for i in reversed(range(city.__len__())):
+            if city[i] == ending[0]:
+                return city[:i] + ending
+
+
+UTF8Writer = codecs.getwriter('utf8')
+sys.stdout = UTF8Writer(sys.stdout)
+
+test = TestPreparer()
+cities = test.start()
+
+plp = PLP()
+plp._init()
+
+s = Stemmer(plp)
+s.load_from_file('trie_only_nouns.bak')
+correct_number = 0
+all_number = 0
+
+for k, v in cities.iteritems():
+    all_number += 1
+    basic_form = ''
+    for city_parts in v.split(' '):
+        basic_form += s.find_basic_form(unicode(city_parts)) + ' '
+
+    basic_form = basic_form.strip()
+    if basic_form != k:
+        print v, k, basic_form
+    else:
+        correct_number += 1
+
+print "Score\n" + str(all_number) + "::" + str(correct_number)
--- Stemmer/stemmer.py
+++ Stemmer/stemmer.py
@@ -1,133 +1,97 @@
-#coding=UTF-8
-__author__ = 'maciej'
+# coding=UTF-8
+__author__ = 'mslawin'
 
-from plp import *
 import marisa_trie
-import sys
-import codecs
+
 
 class Stemmer:
     def __init__(self, plp):
         self.plp = plp
 
-    def writeTrieToFile(self, filename):
-        self.atergoTrie.save(filename)
+    def write_trie_to_file(self, filename):
+        self.atergo_trie.save(filename)
 
-    def loadFromFile(self, filename):
+    def load_from_file(self, filename):
         fmt = '<15s15s15'
-        self.atergoTrie = marisa_trie.RecordTrie(fmt)
-        self.atergoTrie.load(filename)
+        self.atergo_trie = marisa_trie.RecordTrie(fmt)
+        self.atergo_trie.load(filename)
 
-    def prepare(self, onlyNouns):
+    def prepare(self, only_nouns):
         old = ""
         index = 0
         keys = []
         values = []
-        for i in range(16777231,18663982):
-            if onlyNouns == True:
+        for i in range(16777231, 18663982):
+            if only_nouns:
                 if self.plp.label(i)[0] != u'A':
                     continue
-#            if index == 10:
-#                break
-            if self.isBadValue(i):
+            if self.is_bad_value(i):
                 continue
             form = self.plp.bform(i)
 
             if old != form:
-                toRemove = ""
-                toAdd = ""
                 for s in self.plp.forms(i):
                     if len(s) > 0:
                         a = self.substr(s, form)
-                        toRemove = s[len(a) : len(s)]
-                        toAdd = form[len(a) : len(form)]
+                        to_remove = s[len(a): len(s)]
+                        to_add = form[len(a): len(form)]
                         keys.append(self.reverse(s))
-                        a = unicode(toRemove).encode('utf-8')
-                        b = unicode(toAdd).encode('utf-8')
+                        a = unicode(to_remove).encode('utf-8')
+                        b = unicode(to_add).encode('utf-8')
                         values.append((a, b))
-                index += 1                        #najbardziej pasujace slowo, a jak wiecej niz jedno to po count z formEntry
+                index += 1
 
-            old = plp.bform(i)
+            old = self.plp.bform(i)
         fmt = '<15s15s15'
-        self.atergoTrie = marisa_trie.RecordTrie(fmt, zip(keys, values))
+        self.atergo_trie = marisa_trie.RecordTrie(fmt, zip(keys, values))
 
-    def findBasicForm(self, strangeForm):
-        similarWords = self.findSimilarWords(strangeForm)
-        howManyForms = dict()
-        for word in similarWords:
-            form = self.atergoTrie[word]
-            if howManyForms.has_key(form[0]):
-                howManyForms[form[0]] += 1
+    def find_basic_form(self, strange_form):
+        similar_words = self.find_similar_words(strange_form)
+        how_many_forms = dict()
+        for word in similar_words:
+            form = self.atergo_trie[word]
+            if form[0] in how_many_forms:
+                how_many_forms[form[0]] += 1
             else:
-                howManyForms[form[0]] = 1
-        max = 0
-        maxForm = ('', '')
-        for key in howManyForms.keys():
-            if howManyForms[key] > max:
-                max = howManyForms[key]
-                maxForm = key
+                how_many_forms[form[0]] = 1
+        max_forms = 0
+        max_form = ('', '')
+        for key in how_many_forms.keys():
+            if how_many_forms[key] > max_forms:
+                max_forms = how_many_forms[key]
+                max_form = key
 
-        maxForm0 = maxForm[0].split('\x00')[0].decode('utf-8')
-        maxForm1 = maxForm[1].split('\x00')[0].decode('utf-8')
-        return strangeForm[:len(strangeForm) - len(maxForm0)] + maxForm1
+        max_form0 = max_form[0].split('\x00')[0].decode('utf-8')
+        max_form1 = max_form[1].split('\x00')[0].decode('utf-8')
+        return strange_form[:len(strange_form) - len(max_form0)] + max_form1
 
     def substr(self, s1, s2):
         i = 0
         while i < len(s1) and i < len(s2) and s1[i] == s2[i]:
             i += 1
-        return s1[0 : i]
+        return s1[0: i]
 
-    def isBadValue(self, i):
-        return (i <= 16824383 and i >= 16824368) or (i >= 16786128 and i <= 16786143) or \
-               (i <= 16873663 and i >= 16873632) or (i >= 16926016 and i <= 16926031) or \
-               (i <= 17035263 and i >= 17035232) or (i >= 17060080 and i <= 17060095) or \
-               (i <= 17087487 and i >= 17087456) or (i >= 17998800 and i <= 17998815) or \
-               (i <= 18128432 and i >= 18128416) or (i >= 18260592 and i <= 18260607) or \
-               (i <= 18347183 and i >= 18347168)
+    def is_bad_value(self, i):
+        return (16824383 >= i >= 16824368) or (16786128 <= i <= 16786143) or \
+               (16873663 >= i >= 16873632) or (16926016 <= i <= 16926031) or \
+               (17035263 >= i >= 17035232) or (17060080 <= i <= 17060095) or \
+               (17087487 >= i >= 17087456) or (17998800 <= i <= 17998815) or \
+               (18128432 >= i >= 18128416) or (18260592 <= i <= 18260607) or \
+               (18347183 >= i >= 18347168)
 
     def reverse(self, s):
         result = ''
-        i = len(s) -1
-        while (i >= 0):
+        i = len(s) - 1
+        while i >= 0:
             result += s[i]
             i -= 1
         return result
 
-    def findSimilarWords(self, strangeForm):
-        reversedStrangeForm = self.reverse(strangeForm)
+    def find_similar_words(self, strange_form):
+        reversed_strange_form = self.reverse(strange_form)
         index = 0
-        while(index < len(strangeForm) and self.atergoTrie.has_keys_with_prefix( \
-                reversedStrangeForm[:index])):
+        while index < len(strange_form) and self.atergo_trie.has_keys_with_prefix(reversed_strange_form[:index]):
             index += 1
-        return self.atergoTrie.keys(reversedStrangeForm[:index-1])
+        return self.atergo_trie.keys(reversed_strange_form[:index - 1])
 
-
-plp = PLP()
-plp._init()
-
-
-s = Stemmer(plp)
-#s.prepare(True)
-#s.writeTrieToFile('trie_only_nouns.bak')
-s.loadFromFile('trie.bak')
-print '\n###################\n'
-UTF8Writer = codecs.getwriter('utf8')
-sys.stdout = UTF8Writer(sys.stdout)
-
-f = codecs.open('test.txt', 'r', 'utf-8')
-ilePoprawnych = 0
-ileWszystkich = 0
-for line in f:
-    ileWszystkich += 1
-    a = line.split(',')
-    basci_form = s.findBasicForm(unicode(a[0]))
-    x = unicode(a[1])
-    if basci_form != a[1].strip():
-        print (a[0] + "::" + basci_form + "::" + a[1])
-    else:
-        ilePoprawnych += 1
-
-print "Score\n" + str(ileWszystkich) + "::" + str(ilePoprawnych)
-
-while True:
-    print s.findBasicForm(unicode(raw_input(), 'utf-8'))
+        # f = codecs.open('test.txt', 'r', 'utf-8')
